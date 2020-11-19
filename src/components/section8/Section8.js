3ead3ea624162a1363010f47ece5e8222d6a0565
raultocantins <alexbraul.ar@gmail.com>
2020-11-19 11:45:29 -0300

    updating page

--- src/components/section8/Section8.js
+++ src/components/section8/Section8.js
@@ -28,7 +28,7 @@ export default function Section8() {
 
             <ReactPlayer
            controls={true}
-           volume='1'
+           volume={1}
             className="reactplayer"
                 style={{ marginBottom: '30px' }}
                 url="https://www.youtube.com/watch?v=OBAXqWDC0n8&feature=emb_logo"
