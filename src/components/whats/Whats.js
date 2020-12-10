a280db27ae89e70712628d532f4694812c8fc827
raultocantins <alexbraul.ar@gmail.com>
2020-12-10 09:32:59 -0300

    updating 10/12/2020

--- src/components/whats/Whats.js
+++ src/components/whats/Whats.js
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from "react";
 import Whats from "../../assets/whats.jpg";
-import './Whats.css'
-export default function Whatsapp(){
-    return(
+import "./Whats.css";
+export default function Whatsapp() {
+  return (
+    <div>
+      <a href="https://api.whatsapp.com/send/?phone=5563992432379&text&app_absent=0">
         <section className="whatsapp">
-            <a
-        href="https://api.whatsapp.com/send/?phone=5563992432379&text&app_absent=0"
-        
-      >
-    
-        <img src={Whats} alt="whats" />
-      </a>
-      <span>Duvidas? Chame no WhatsApp</span>
+          <img src={Whats} alt="whats" />
+          <span>Duvidas? Chame no WhatsApp</span>
         </section>
-    )
+      </a>
+    </div>
+  );
 }
